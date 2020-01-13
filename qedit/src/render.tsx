4a422ec8ceaaf9d68a04f6ca6318393e6c72be2f
S.Percentage <Syn.Tri.Naga@gmail.com>
2020-01-14 00:27:10 +0900

    add: base editing ui

--- qedit/src/render.tsx
+++ qedit/src/render.tsx
@@ -3,11 +3,51 @@ import "./style.sass";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-function AppMain(): JSX.Element
+function QuestionEditorPopup(): JSX.Element
 {
     return <section className="popup">
-        <h1>AppMain</h1>
+        <h1>新しいクイズ</h1>
+        <div className="container">
+            <form>
+                <div className="row">
+                    <label htmlFor="q_id">ID:&nbsp;</label>
+                    <input type="input" id="q_id" />
+                </div>
+                <div className="row twoliner">
+                    <label htmlFor="q_text">クイズ内容:</label>
+                    <textarea id="q_text" rows={5}></textarea>
+                </div>
+                <div className="subgroup">
+                    <header>
+                        <h3>選択肢:</h3>
+                        <button type="button">＋</button>
+                    </header>
+                    <ul>
+                        <li>
+                            <input type="radio" className="inline" />
+                            <input type="text" className="inline expanded" />
+                            <button type="button">×</button>
+                        </li>
+                        <li>
+                            <input type="radio" className="inline" />
+                            <input type="text" className="inline expanded" />
+                            <button type="button">×</button>
+                        </li>
+                    </ul>
+                </div>
+                <div className="row right">
+                    <button type="submit">作成</button>
+                </div>
+            </form>
+        </div>
     </section>;
+}
+
+function AppMain(): JSX.Element
+{
+    return <div>
+        <QuestionEditorPopup />
+    </div>;
 }
 
 ReactDOM.render(<AppMain />, document.getElementById("app"));
