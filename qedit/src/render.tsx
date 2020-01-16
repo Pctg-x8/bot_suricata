8f69d5428d36d92142e5d20e5c8e7c04dea7a98f
S.Percentage <Syn.Tri.Naga@gmail.com>
2020-01-17 00:12:54 +0900

    add: question table mock

--- qedit/src/render.tsx
+++ qedit/src/render.tsx
@@ -107,9 +107,31 @@ function QuestionEditorPopup(props: EditorPopupProperties): JSX.Element
     </section>;
 }
 
+function QuestionList(): JSX.Element
+{
+    return <section id="qlist">
+        <div className="head numcenter">ID</div>
+        <div className="head">設問</div>
+        <div className="head numcenter">選択肢</div>
+
+        <div className="numcenter">999</div>
+        <div>あああ<br />あああああ</div>
+        <div className="numcenter">2</div>
+
+        <div className="numcenter">998</div>
+        <div>あああ<br />あいいいい<br />テスト</div>
+        <div className="numcenter">3</div>
+
+        <div className="numcenter">10524</div>
+        <div>テストですの</div>
+        <div className="numcenter">8</div>
+    </section>;
+}
+
 function AppMain(): JSX.Element
 {
     return <div>
+        <QuestionList />
         <QuestionEditorPopup create={true} />
     </div>;
 }
